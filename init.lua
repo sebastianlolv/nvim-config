6758d083de46974f68bf69911e468c5be7f1786a
Sebastian Hana <43113824+sebastianlolv@users.noreply.github.com>
2025-05-12 15:23:38 +0200

    chore: remove old stuff

--- init.lua
+++ init.lua
@@ -97,12 +97,6 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
 	-- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
 	"tpope/vim-sleuth", -- Detect tabstop and shiftwidth automatically
-	"vim-syntastic/syntastic",
-	"hashivim/vim-terraform",
-	"juliosueiras/vim-terraform-completion",
-	"Shougo/deoplete.nvim",
-	"roxma/nvim-yarp",
-	"roxma/vim-hug-neovim-rpc",
 
 	-- NOTE: Plugins can also be added by using a table,
 	-- with the first argument being the link and the following
