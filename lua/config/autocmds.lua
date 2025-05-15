f7a2576fda0c5a29644f039d0e4b83270d45a10f
Sebastian Hana <43113824+sebastianlolv@users.noreply.github.com>
2025-05-15 14:37:53 +0200

    test

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -113,7 +113,7 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
 	end,
 })
 
--- Auto create dir when saving a file, in case some intermediate directory does not exist
+-- Auto create dir when saving a file, in case some intermediate directory does not exist.
 vim.api.nvim_create_autocmd({ "BufWritePre" }, {
 	group = augroup("auto_create_dir"),
 	callback = function(event)
