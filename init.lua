99285d2b0293e36a4c8bd22f1574c306b9dfeae8
Sebastian Hana <43113824+sebastianlolv@users.noreply.github.com>
2025-05-15 10:09:54 +0200

    chore: packer.nvim

--- init.lua
+++ init.lua
@@ -71,6 +71,13 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 	end,
 })
 
+vim.cmd([[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost core.lua source <afile> | PackerCompile
+  augroup end
+]])
+
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
